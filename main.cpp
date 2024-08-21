e52607b9a7c50c04aa160d6b3687396abbb15e43
Luigi <gigi.demarco@hotmail.it>
2024-08-21 11:02:39 +0100

    added model view proj matrices, added camera control with WASDRF

--- main.cpp
+++ main.cpp
@@ -12,8 +12,11 @@
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+float translationSpeed = 0.001f;
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
+void cameraControl(GLFWwindow* window, glm::vec3 &translationVector);
 
 
 int main() {
@@ -156,24 +159,43 @@ int main() {
      }
      stbi_image_free(data);
 
+     //camera movement
+     glm::vec3 translationVector;
 
      //transformations
 
      unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
      
+     glm::mat4 model = glm::mat4(1.0f);
+     model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 
+     glm::mat4 view = glm::mat4(1.0f);
+     view = glm::translate(view, glm::vec3(0.0f, 0.0f, -1.0f));
 
+     glm::mat4 projection;
+     projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
 
     while (!glfwWindowShouldClose(window))
     {
+        double timeElapsed = glfwGetTime();
         //input
         processInput(window);
 
-        //transformations
-        glm::mat4 trans = glm::mat4(1.0f);
-        trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
-        trans = glm::rotate(trans, float(glfwGetTime()), glm::vec3(0.0, 0.0, 1.0));
-        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
+        //camera stuff
+        translationVector = glm::vec3(0.0f);
+        cameraControl(window, translationVector);
+        view = glm::translate(view, translationVector);
+
+        //model view proj matrixes update
+        int modelLoc = glGetUniformLocation(ourShader.ID, "model");
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+        int viewLoc = glGetUniformLocation(ourShader.ID, "view");
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+
+        int projectionLoc = glGetUniformLocation(ourShader.ID, "projection");
+        glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
 
         // rendering
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
@@ -203,4 +225,31 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 void processInput(GLFWwindow* window) {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+
+}
+
+void cameraControl(GLFWwindow* window, glm::vec3& translationVector) {
+    // Translation along the Z-axis
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+        translationVector.z += translationSpeed;
+    }
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+        translationVector.z -= translationSpeed;
+    }
+
+    // Translation along the X-axis
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+        translationVector.x += translationSpeed;
+    }
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+        translationVector.x -= translationSpeed;
+    }
+
+    // Translation along the Y-axis
+    if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS) {
+        translationVector.y -= translationSpeed;
+    }
+    if (glfwGetKey(window, GLFW_KEY_F) == GLFW_PRESS) {
+        translationVector.y += translationSpeed;
+    }
 }
